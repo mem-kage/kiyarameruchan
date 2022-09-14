6a28df3e27e85eabf17ec5f33983970a2c5af9e8
kagehira shota <kagehira.shota.2459@members.co.jp>
2022-09-14 10:04:29 +0900

    [add]占いの結果画面のCSS追加

--- src/components/Uranai/result.tsx
+++ src/components/Uranai/result.tsx
@@ -19,55 +19,57 @@ const UranaiResult: React.VFC<Props> = ({uranaiFlg, setUranaiFlg, setIsLoading, 
     },
     {
       "item": {
-        "result": "マジでやばい！！",
-        "resultTxt": "いいことが起こりすぎて周りから嫉妬されるかも...?"
+        "result": "絶好調フィーバー:ピカピカ:",
+        "resultTxt": "いいことが起こりすぎてハッピー"
       }
     },
     {
       "item": {
-        "result": "マジでやばい！！",
-        "resultTxt": "いいことが起こりすぎて周りから嫉妬されるかも...?"
+        "result": "絶好調！！",
+        "resultTxt": "今日はいつもよりいい日になるめる！自信持って今日も頑張るめる"
       }
     },
     {
       "item": {
-        "result": "マジでやばい！！",
-        "resultTxt": "いいことが起こりすぎて周りから嫉妬されるかも...?"
+        "result": "好調",
+        "resultTxt": "何かいいことがあるかも！？"
       }
     },
     {
       "item": {
-        "result": "マジでやばい！！",
-        "resultTxt": "いいことが起こりすぎて周りから嫉妬されるかも...?"
+        "result": "普通",
+        "resultTxt": "特に何もない平凡な日になるめる"
       }
     },
     {
       "item": {
-        "result": "マジでやばい！！",
-        "resultTxt": "いいことが起こりすぎて周りから嫉妬されるかも...?"
+        "result": "不調",
+        "resultTxt": "なんだか嫌な予感...がするめる。。。甘いものを食べてリフレッシュするめる！"
       }
     },
     {
       "item": {
-        "result": "マジでやばい！！",
-        "resultTxt": "いいことが起こりすぎて周りから嫉妬されるかも...?"
+        "result": "絶不調",
+        "resultTxt": "今日は嫌なことがあるめる。周りを注意したほうがいいめる。"
       }
     }
   ]
   console.log(result[0].item.result)
 
   return (
-    <div className="uranai-result-wrapper">
-      <h3>占い後</h3>
-      <p>{result[selectedNumber-1].item.result}</p>
-      <UranaiBtn
-        status={uranaiFlg}
-        setStatus={setUranaiFlg}
-        btnTxt={"もう一度占うめる"}
-        setIsLoading={setIsLoading}
-      />
-    </div>
+    <Container>
+      <div className="uranai-result">
+        <p>{result[selectedNumber-1].item.result}</p>
+        <p>{result[selectedNumber-1].item.resultTxt}</p>
+      </div>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  background: url(${process.env.PUBLIC_URL}/images/uranai/kc_uranai_result.svg) center center / contain no-repeat;
+  height: 100vh;
+  position: relative;
+`;
 
 export default UranaiResult;
