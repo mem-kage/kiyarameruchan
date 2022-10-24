1e540c32b9180ca29310fb35ec314d6a247e0813
kagehira shota <kagehira.shota.2459@members.co.jp>
2022-10-24 13:25:06 +0900

    [add]react henlmet async

--- src/components/parts/CommonSNS.tsx
+++ src/components/parts/CommonSNS.tsx
@@ -5,6 +5,7 @@ import {
   LineShareButton,
   LineIcon
 } from "react-share";
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 interface Props {
   twURL: string;
@@ -17,20 +18,36 @@ const CommonSNS: React.VFC<Props> = ({twURL, title}: Props) => {
   const sendURL = `https://line.me/R/share?text=${encodeTitle}`;
 
   return(
-    <div className="common-sns">
-      <TwitterShareButton
-        url={twURL}
-        title={title}
-      >
-        <TwitterIcon size={40} round />
-      </TwitterShareButton>
+    <>
+      <HelmetProvider>
+        <Helmet
+          title={`きやらめるてスト`}
+          meta={[
+              { name: 'twitter:card', content: 'summary' },
+              { property: 'og:title', content: 'きやらめるてスト' },
+              { property: 'og:type', content: 'website' },
+              { property: 'og:url', content: twURL },
+              { property: 'og:image', content: "https://mem-kage.github.io/kiyarameruchan/images/sindan/result/kc_ps_item_03.png" },
+              { property: 'og:description', content: 'テストテスト' },
+          ]}
+        />
+      </HelmetProvider>
 
-      {/* <LineShareButton url={twURL} title={title} >
-        <LineIcon size={40} round />
-      </LineShareButton> */}
-      <a target="_blank" href={sendURL} rel="noreferrer">ラインで送る</a>
+      <div className="common-sns">
+        <TwitterShareButton
+          url={twURL}
+          title={title}
+        >
+          <TwitterIcon size={40} round />
+        </TwitterShareButton>
 
-    </div>
+        <LineShareButton url={twURL} title={title} >
+          <LineIcon size={40} round />
+        </LineShareButton>
+        <a target="_blank" href={sendURL} rel="noreferrer">ラインで送る</a>
+
+      </div>
+    </>
   )
 }
 
