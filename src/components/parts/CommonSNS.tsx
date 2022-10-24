c864422709698570043e6be6fcc9b68a9bce83ab
kagehira shota <kagehira.shota.2459@members.co.jp>
2022-10-24 12:02:53 +0900

    [fix]line

--- src/components/parts/CommonSNS.tsx
+++ src/components/parts/CommonSNS.tsx
@@ -25,8 +25,9 @@ const CommonSNS: React.VFC<Props> = ({twURL, title}: Props) => {
       <LineShareButton url={twURL} title={title} >
         <LineIcon size={40} round />
       </LineShareButton>
-    </div>
+      <a target="_blank" href="https://line.me/R/share?text=%E3%81%93%E3%82%93%E3%81%AB%E3%81%A1%E3%81%AF" rel="noreferrer">ラインで送る</a>
 
+    </div>
   )
 }
 
